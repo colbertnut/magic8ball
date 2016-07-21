c6c241f7c04ce2ac944d4b693ffa3597f9f46320
colbynut <colby@overstock.com>
2016-07-20 19:22:47 -0600

    firt commit

---
+++ main.js
@@ -0,0 +1,24 @@
+/*function clearAll() {
+val = 0;
+operator = 0;
+total = 0;
+input.value = 0;
+label.innerHTML = 0;
+
+}
+
+var total = 0;
+var operator;
+var val;
+
+var label = document.getElementById('total');
+var input = document.getElementById('input');
+input.value = 0;
+label.innerHTML = total;
+
+function magicEightBall() {
+	val = 0;
+	input.value = 0;
+	label.innerHTML = 0;
+
+}
